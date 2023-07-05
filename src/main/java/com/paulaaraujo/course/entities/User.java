6dc62062e00480298fb61872e710d51ba66a9cad
Paula Priscila da Cruz Araujo-PAULAPCR <paulapcr@motorola.com>
2023-07-05 12:22:41 -0300

    H2 database, test profile, JPA

--- src/main/java/com/paulaaraujo/course/entities/User.java
+++ src/main/java/com/paulaaraujo/course/entities/User.java
@@ -1,20 +1,28 @@
 package com.paulaaraujo.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
-	// add basic atributes
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
-	//add a constructor
-	public User() {}
+
+	public User() {
+	}
 
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
@@ -24,9 +32,7 @@ public class User implements Serializable {
 		this.phone = phone;
 		this.password = password;
 	}
-	
-	
-    //add getters e setters
+
 	public Long getId() {
 		return id;
 	}
@@ -66,11 +72,13 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	//add comparation
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -82,13 +90,11 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
-	//add serializable
-	
-	
-	
 }
-
